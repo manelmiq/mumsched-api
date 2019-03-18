9f37b05ed463244bcfb66dbaba8436da48581cf1
Adriano Marra <adrianomarra90@gmail.com>
2019-03-17 22:42:52 -0500

    Update Laravel version and configure CORS

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Application Service Providers...
